26d9d09f8f9f1a8628aa3bf94c862c4bb1d8489b
Ales Puchilo <ales.puchilo@gmail.com>
2022-03-14 15:49:14 +0100

    Edited to cyber

--- python/scripts/proto-gen.sh
+++ python/scripts/proto-gen.sh
@@ -4,9 +4,9 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 
 echo "install betterproto... pre-release for now. stable one has some issues"
 #pip install --upgrade "avast.betterproto[compiler]"
-pip install --upgrade "betterproto[compiler]" --pre
+pip3 install --upgrade "betterproto[compiler]" --pre
 
-OUT_DIR="./terra_proto"
+OUT_DIR="./cyber_proto"
 
 mkdir -p "$OUT_DIR"
 
@@ -14,10 +14,11 @@ echo "Processing terrad proto files ..."
 
 TERRAD_DIR="../terrad/proto"
 TERRAD_THIRD_PARTY_DIR="../terrad/third_party/proto"
-
+CYBER_DIR="../go-cyber/proto"
 
 protoc \
   --proto_path=${TERRAD_DIR} \
   --proto_path=${TERRAD_THIRD_PARTY_DIR} \
+  --proto_path=${CYBER_DIR} \
   --python_betterproto_out="${OUT_DIR}" \
   $(find ${TERRAD_DIR} ${TERRAD_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
