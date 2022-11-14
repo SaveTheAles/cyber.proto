347ed354f247a00160e0397664870b1ddd7a0176
Snedashkovsky <sn@cyberdrop.ai>
2022-11-15 00:14:48 +0800

    - update proto gen script - update tendermint types - add grpc proto

--- python/scripts/proto-gen.sh
+++ python/scripts/proto-gen.sh
@@ -22,9 +22,17 @@ LIQUIDITY_THIRD_PARTY_DIR="../liquidity/third_party/proto"
 protoc \
   --proto_path=${TERRAD_DIR} \
   --proto_path=${TERRAD_THIRD_PARTY_DIR} \
+  --python_betterproto_out="${OUT_DIR}" \
+  $(find ${TERRAD_DIR} ${TERRAD_THIRD_PARTY_DIR}  -path -prune -o -name '*.proto' -print0 | xargs -0)
+
+protoc \
   --proto_path=${LIQUIDITY_DIR} \
   --proto_path=${LIQUIDITY_THIRD_PARTY_DIR} \
+  --python_betterproto_out="${OUT_DIR}" \
+  $(find ${LIQUIDITY_THIRD_PARTY_DIR} ${LIQUIDITY_DIR}  -path -prune -o -name '*.proto' -print0 | xargs -0)
+
+protoc \
   --proto_path=${CYBER_DIR} \
   --proto_path=${CYBER_THIRD_PARTY_DIR} \
   --python_betterproto_out="${OUT_DIR}" \
-  $(find ${TERRAD_DIR} ${TERRAD_THIRD_PARTY_DIR} ${LIQUIDITY_THIRD_PARTY_DIR} ${LIQUIDITY_DIR} ${CYBER_DIR} ${CYBER_THIRD_PARTY_DIR}  -path -prune -o -name '*.proto' -print0 | xargs -0)
+  $(find ${CYBER_DIR} ${CYBER_THIRD_PARTY_DIR}  -path -prune -o -name '*.proto' -print0 | xargs -0)
